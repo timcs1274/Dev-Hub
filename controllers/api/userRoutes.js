2d00344aaf5ac586644a77463754554cea58653c
priyarizal <103598087+priyarizal@users.noreply.github.com>
2022-08-03 14:01:08 -0400

    base MVC directory structure

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const { User } = require('../../models'); //dont understand 
+
+
+
+//post route to create user's login
+//post routes for login 
+//within the post route to login - check if the email and password are valid (checkpassword)
+//logout post route (destroy)
---
+++ server.js
@@ -0,0 +1,4 @@
+// handlebars
+//npm library session
+//sequelize
+//
