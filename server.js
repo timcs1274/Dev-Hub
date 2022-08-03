5c9a771256988f91048e5356ed7d4e4de008c867
priyarizal <103598087+priyarizal@users.noreply.github.com>
2022-08-03 14:05:06 -0400

    jotted down libraries we probably need to install

--- server.js
+++ server.js
@@ -2,3 +2,15 @@
 //npm library session
 //sequelize
 //
+
+
+//packages to download: 
+//eslint
+//express
+// sequelize
+//mysql2
+//handlebars
+//express-session
+//connect-session-sequelize
+//dotenv 
+//bcrypt
