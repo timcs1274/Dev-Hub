2b486afcbb6abbba6e196c63af64e53d66eb74fb
priyarizal <103598087+priyarizal@users.noreply.github.com>
2022-08-04 16:32:10 -0400

    friendslist and techinterest model needed changes

---
+++ models/friendslist.js
@@ -0,0 +1,31 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class friendsList extends Model {
+   //probably have to check password here (login)
+    //then return a bcrypted password and compare it with signup info
+}
+
+friendsList.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      references :{
+        model: 'user',
+        key: 'id'
+      }
+    },
+  }
+);
+
+//sequelize built-in property
+
+module.exports = friendsList;
--- models/techinterests.js
+++ models/techinterests.js
@@ -3,38 +3,28 @@ const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
 class techInterest extends Model {
-   //probably have to check password here (login)
+    //probably have to check password here (login)
     //then return a bcrypted password and compare it with signup info
 }
 
 techInterest.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        technologies: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            references : {
+                model: 'user',
+                key: 'id'
+            }
+        },
+
     },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true,
-      },
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [8],
-      },
-    },
-  }
 );
 
 //sequelize built-in property
