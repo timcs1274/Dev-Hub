dd77f1fa65b6a7db253b1c0ff651b241c39ae81a
Yahir Federico <107011404+Yahir-F@users.noreply.github.com>
2022-08-04 12:47:00 -0700

    created login behavior

---
+++ login.js
@@ -0,0 +1,21 @@
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const email = document.querySelector('#InputEmail').value.trim();
+    const password = document.querySelector('#PasswordInput').value.trim();
+  
+    if (email && password) {
+      const response = await fetch('', {
+        method: 'POST',
+        body: JSON.stringify({ email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert('Failed to log in');
+      }
+    }
+  };
+  
