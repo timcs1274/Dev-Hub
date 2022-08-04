259a2a8bbb3f23d546979984d79e3d4937c27083
priyarizal <103598087+priyarizal@users.noreply.github.com>
2022-08-04 14:56:35 -0400

    init helpers/handlebars

--- models/User.js
+++ models/User.js
@@ -37,5 +37,6 @@ User.init(
     },
 )
 
+//add hooks for beforecreate and beforeUpdate with .hash and the allowed length of passowrd 
 
 module.exports = User;
--- server.js
+++ server.js
@@ -14,3 +14,33 @@
 //connect-session-sequelize
 //dotenv 
 //bcrypt
+
+//initializing express and writing down the port number to listen to + 
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
+  
+  app.use(session(sess));
+  
+  // Inform Express.js on which template engine to use
+  app.engine('handlebars', hbs.engine);
+  app.set('view engine', 'handlebars');
+  
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.static(path.join(__dirname, 'public')));
+  
+  app.use(routes);
+  
+  sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+  });
