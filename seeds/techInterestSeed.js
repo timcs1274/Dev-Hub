ad7222aee7224e94cfd48903445f2aef18cc77f3
priyarizal <103598087+priyarizal@users.noreply.github.com>
2022-08-05 16:22:46 -0400

    edit technologies seed

--- seeds/techInterestSeed.js
+++ seeds/techInterestSeed.js
@@ -4,20 +4,9 @@ const techInterestSeedData =
 [
     {
         user_id: 2,
-        content: "Yes! I've drank tea with honey and lemon for a couple of years and it cured my depression",
-        post_id: 1
+        technologies: "REST API's"
         
     },
-    {
-        user_id: 3,
-        content: "I would love to go to space one day.",
-        post_id: 2
-    },
-    {
-        user_id: 2,
-        content: "Mid post bro!",
-        post_id: 3
-    }
 ]
 
 const seedingTechInterest = () => techInterest.bulkCreate(techInterestSeedData)
