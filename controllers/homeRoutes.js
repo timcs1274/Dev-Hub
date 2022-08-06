f292a6c7c6c43d5e928314d9f4fc4f911bd6edbc
priyarizal <103598087+priyarizal@users.noreply.github.com>
2022-08-06 16:13:49 -0400

    profile route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -34,5 +34,23 @@ router.get('/login', async(req, res) => {
     }
 });
 
+router.get('/profile', withAuth, async (req, res) => {
+    try {
+      // Find the logged in user based on the session ID
+      const userData = await User.findByPk(req.session.user_id, {
+        attributes: { exclude: ['password'] },
+        include: [{ model: Project }],
+      });
+  
+      const user = userData.get({ plain: true });
+  
+      res.render('profile', {
+        ...user,
+        logged_in: true
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 module.exports = router;
