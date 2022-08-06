57359ad123db51bdd19ed8333030c58a03f0034b
DNA601 <dnavarrete6001@gmail.com>
2022-08-06 06:54:21 -0700

    worked on the login routes to populate page

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,10 @@
 const router = require('express').Router();
-// const apiRoutes = require('./api');
-// const homeRoutes = require('./homeRoutes.js');
-// router.use('/api', apiRoutes);
-// router.use('/', homeRoutes);
-router.get("/", (req, res) => {
-    res.send("Works -vince")
-}) 
+const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes');
+
+router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
+
 router.use((req, res) => {
     res.status(404).end();
 });
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const mysql = require('mysql2');
-const hbs = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const bcrypt = require('bcrypt');
 const session = require('express-session')
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -37,22 +37,24 @@ const sess = {
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
-      db: sequelize
+        db: sequelize
     })
-  };
-  
-  app.use(session(sess));
-  
-  // Inform Express.js on which template engine to use
-  app.engine('handlebars', hbs.engine);
-  app.set('view engine', 'handlebars');
-  
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-  app.use(express.static(path.join(__dirname, 'public')));
-  
-  app.use(routes);
-  
-  sequelize.sync({ force: false }).then(() => {
+};
+
+app.use(session(sess));
+
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create();
+// Inform Express.js on which template engine to use
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
-  });
+});
