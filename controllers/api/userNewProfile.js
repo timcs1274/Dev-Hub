0eb1a15e3771b5a29ba11a1c6a3e34a187980d92
timcs <timcs1274@gmail.com>
2022-08-06 15:49:28 -0400

    create createproject route

--- controllers/api/userNewProfile.js
+++ controllers/api/userNewProfile.js
@@ -1,11 +1,17 @@
-const { appendFile } = require('fs');
-
 const router = require('express').Router();
+const { techinterests, Friends, User } = require('../models');
+const cloudinary = require("cloudinary");
 
 
+router.post('/homepage.handlebars', (req, res) => {
+        console.log('testing yo yoyoyoyoyoyo')
+})
 app.post('', (req, res) => {
 
 });
 
 
 module.exports = router;
+
+
+
