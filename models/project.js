e88c0c9b5f9808875323d6de8cdd2e2a436255ac
priyarizal <103598087+priyarizal@users.noreply.github.com>
2022-08-05 22:51:26 -0400

    seeds needed some changes. now runs without error

--- models/project.js
+++ models/project.js
@@ -19,11 +19,6 @@ Project.init(
     description: {
       type: DataTypes.STRING,
     },
-    date_created: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      defaultValue: DataTypes.NOW,
-    },
     user_id: {
       type: DataTypes.INTEGER,
       references: {
