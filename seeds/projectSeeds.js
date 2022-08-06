2ab07ff43f5caf0cdfddb9cfd7fca1f200a15d1c
priyarizal <103598087+priyarizal@users.noreply.github.com>
2022-08-06 16:14:45 -0400

    forgot name on seed

--- seeds/projectSeeds.js
+++ seeds/projectSeeds.js
@@ -1,29 +1,28 @@
 const { project } = require('../models');
 
-const projectSeedData = 
+const projectSeedData =
 
-[
-  {
-    user_id : 1,
-    name: "RubyonRails",
-    content: "A new scientific dicover suggests that the natural ingredients of honey and Vitamin A,,C, and B6 found in lemon are proven to cure depression!",
-    
-  },
-  {
-    user_id : 2,
-    name: "Bootstrap and CSS",
-    description: "Remember the super popular movie Interstellar? Well.. the whole thing about not aging in space is true. Technically, you could age reall really reallyyy** slow if you traveled at the speed of light",
-    
-    
-  },
-  {
-    user_id : 3,
-    name: "",
-    description: "Touching grass is imperative to mental and physical health. Touching grass 5 times a day, keeps depression away!", 
-    
-  }
-]
-  
+  [
+    {
+      user_id: 1,
+      name: "RubyonRails1",
+      content: "A new scientific dicover suggests that the natural ingredients of honey and Vitamin A,,C, and B6 found in lemon are proven to cure depression!",
+    },
+    {
+      user_id: 2,
+      name: "Bootstrap and CSS",
+      description: "Remember the super popular movie Interstellar? Well.. the whole thing about not aging in space is true. Technically, you could age reall really reallyyy** slow if you traveled at the speed of light",
+
+
+    },
+    {
+      user_id: 3,
+      name: "Touch Grass",
+      description: "Touching grass is imperative to mental and physical health. Touching grass 5 times a day, keeps depression away!",
+
+    }
+  ]
+
 const seedingProject = () => project.bulkCreate(projectSeedData)
 
 module.exports = seedingProject;
