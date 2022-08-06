8719cdf3a2c63637911860f3b8ab9b729c7f2bbe
DNA601 <dnavarrete6001@gmail.com>
2022-08-06 13:41:27 -0700

    pair programming for login and logout feature. debugging

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,7 +1,20 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 // const cloudinary = require("cloudinary");
+router.post('/', async(req, res) => {
+    try {
+        const userData = await User.create(req.body);
 
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+
+            res.status(200).json(userData);
+        });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
 
 
 //post route to create user's login
@@ -49,6 +62,17 @@ router.post('/login', async(req, res) => {
 //     Image.create(image) // save image information in database
 //         .then(newImage => res.json(newImage))
 //         .catch(err => console.log(err));
+
 // });
+router.post('/logout', (req, res) => {
+    if (req.session.logged_in) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+
 
 module.exports = router;
--- public/js/login.js
+++ public/js/login.js
@@ -1,51 +1,51 @@
-// const loginFormHandler = async(event) => {
-//     event.preventDefault();
+const loginFormHandler = async(event) => {
+    event.preventDefault();
 
-//     const email = document.querySelector('#InputEmail').value.trim();
-//     const password = document.querySelector('#PasswordInput').value.trim();
+    const email = document.querySelector('#InputEmail').value.trim();
+    const password = document.querySelector('#PasswordInput').value.trim();
 
-//     if (email && password) {
-//         const response = await fetch('/api/users/login', {
-//             method: 'POST',
-//             body: JSON.stringify({ email, password }),
-//             headers: { 'Content-Type': 'application/json' },
-//         });
+    if (email && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
 
-//         if (response.ok) {
-//             document.location.replace('/');
-//         } else {
-//             alert('Failed to log in');
-//         }
-//     }
-// };
-// const signupFormHandler = async(event) => {
-//     event.preventDefault();
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Failed to log in');
+        }
+    }
+};
+const signupFormHandler = async(event) => {
+    event.preventDefault();
 
-//     const name = document.querySelector('#name-signup').value.trim();
-//     const email = document.querySelector('#email-signup').value.trim();
-//     const password = document.querySelector('#password-signup').value.trim();
+    const name = document.querySelector('#name-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
 
-//     if (name && email && password) {
-//         const response = await fetch('/api/users', {
-//             method: 'POST',
-//             body: JSON.stringify({ name, email, password }),
-//             headers: { 'Content-Type': 'application/json' },
-//         });
+    if (name && email && password) {
+        const response = await fetch('/api/users', {
+            method: 'POST',
+            body: JSON.stringify({ name, email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
 
-//         if (response.ok) {
-//             document.location.replace('/');
-//         } else {
-//             alert(response.statusText);
-//         }
-//     }
-// };
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
 
 
 
-// document
-//     .querySelector('.signup-form')
-//     .addEventListener('submit', signupFormHandler);
+document
+    .querySelector('.signup-form')
+    .addEventListener('submit', signupFormHandler);
 
-// document
-//     .querySelector('.login-form')
-//     .addEventListener('submit', loginFormHandler);
+document
+    .querySelector('.login-form')
+    .addEventListener('submit', loginFormHandler);
