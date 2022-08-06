d1432cda9bcd1ff60cc42acd41e25547e4ce7231
priyarizal <103598087+priyarizal@users.noreply.github.com>
2022-08-06 16:16:33 -0400

    errors in required dependencies

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+const userRoutes = require ('./api/userRoutes')
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
+
 
 router.use((req, res) => {
     res.status(404).end();
