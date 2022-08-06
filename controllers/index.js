1177e4a744a05eb2989ad89219cb1303dcd8e064
Yahir Federico <107011404+Yahir-F@users.noreply.github.com>
2022-08-06 10:31:35 -0700

    set router

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+const projectRoutes = require('./projectRoutes');
 
+
+router.use('/projects', projectRoutes)
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
 
