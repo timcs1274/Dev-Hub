8828aba6d9ad55eba992cefef9562635e9637cbb
priyarizal <103598087+priyarizal@users.noreply.github.com>
2022-08-06 16:10:35 -0400

    changed routes images

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 const userRoutes = require('./userRoutes')
 
-router.use('/user', userRoutes)
+// router.use('/user', userRoutes)
+router.use('/images', userRoutes)
 
 module.exports = router;
